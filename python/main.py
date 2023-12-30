2cf88b171c188d6771da6d0b4925691c1170ec72
5talin 5 <59964372+5talinFive@users.noreply.github.com>
2023-12-30 11:56:34 -0500

    Add files via upload
    segundo commit

--- python/main.py
+++ python/main.py
@@ -14,6 +14,14 @@ def list_clients():
     global clients
     print(clients)
     
+def update_client(client_name, update_client_name):
+    global clients
+    
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', update_client_name + ',')
+    else:
+        print('Client is not in Client List')
+    
     
 def _add_comma():
     global clients
@@ -24,19 +32,29 @@ def _print_welcome():
     print('*' * 25)
     print('What would you like to do today?:')
     print('[C]reate Client')
+    print('[U]pdate Client')
     print('[D]elete Client')
+    
+def _get_client_name():
+    return input('Whta is the client name?')
     
     
 if __name__ == '__main__':
     _print_welcome()
     command = input()
+    command = command.upper()
     
     if command == 'C':
-        client_name = input('What is the client name?: ')
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
     elif command == 'D':
         pass
+    elif command == 'U':
+        client_name = _get_client_name()
+        update_client_name = input('Whta is the update client name?')
+        update_client(client_name, update_client_name)
+        list_clients()
     else:
         print('Invalid Command')
     
