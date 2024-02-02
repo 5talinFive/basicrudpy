7fd694c394027c634132dcf21bafdf69e5d61552
5talin 5 <59964372+5talinFive@users.noreply.github.com>
2024-02-02 17:48:35 -0500

    Add files via upload
    actualización: while loop

---
+++ python/listas.py
@@ -0,0 +1,13 @@
+countries = ['Mexico', 'Venezuela', 'Colombia', 'Argentina']
+ages = ['18', '14', '34', '50']
+
+receta = ['ensalada', 2, 'lechugas', 2, 'ajitomates']
+print(countries)
+
+countries[0] = 'Ecuador'
+print(countries)
+
+
+
+
+
--- python/main.py
+++ python/main.py
@@ -1,4 +1,6 @@
-clients = 'pablo, ricardo, '
+import sys 
+
+clients = 'pablo, ricardo, stalin, '
 
 def create_client(client_name):
     global clients
@@ -21,6 +23,22 @@ def update_client(client_name, update_client_name):
         clients = clients.replace(client_name + ',', update_client_name + ',')
     else:
         print('Client is not in Client List')
+        
+def delete_client(client_name):
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', '')
+    else:
+        print('Client is not in Client List')
+    
+        
+def search_client(client_name):
+        clients_list = clients.split(',')
+        for client in clients_list:
+            if client != client_name:
+                continue
+            else:
+                return True
     
     
 def _add_comma():
@@ -34,12 +52,30 @@ def _print_welcome():
     print('[C]reate Client')
     print('[U]pdate Client')
     print('[D]elete Client')
+    print('[S]earch Cleint')
+    
+    print()
+    print('***** C L I E N T S *****')
+    list_clients()
     
 def _get_client_name():
-    return input('Whta is the client name?')
+    client_name = None
+    
+    while not client_name:
+        client_name = input('Whta is the client name?').strip()
+        
+        if client_name == 'exit':
+            client_name = None
+            break
+        
+    if not client_name:
+            sys.exit()
+    
+    return client_name
     
     
 if __name__ == '__main__':
+    
     _print_welcome()
     command = input()
     command = command.upper()
@@ -49,12 +85,21 @@ if __name__ == '__main__':
         create_client(client_name)
         list_clients()
     elif command == 'D':
-        pass
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
     elif command == 'U':
         client_name = _get_client_name()
         update_client_name = input('Whta is the update client name?')
         update_client(client_name, update_client_name)
         list_clients()
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+        if found:
+            print('The client is in the client list')
+        else:
+            print('The client: {} is not in our client list'.format(client_name))
     else:
         print('Invalid Command')
     
